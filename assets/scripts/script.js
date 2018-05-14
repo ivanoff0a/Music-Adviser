953f41046a95cd1fbb2f83a43d6b537b39094186
bananaWeb <a89.volkov@gmail.com>
2018-05-14 21:16:14 +0300

    v.4 yeah boii

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,5 @@
 $(document).ready(function() {
 
-    // let random = Math.floor(Math.random() * (7 - 1) + 1);
-
-    // $(".section-main").css({
-    //     "background-image": "url(./assets/images/" + random + ".jpg)"
-    // })
-
-
     $(".owl-carousel").owlCarousel({
         items: 1,
         autoHeight: true,
@@ -22,7 +15,6 @@ $(document).ready(function() {
     });
 
         $(".to-search").click(function () {
-          // mobileMenu.removeClass("-opened");
         var id  = $('.section-search'),
             top = $(id).offset().top;
         $('body,html').animate({scrollTop: top}, 1500);
@@ -38,13 +30,8 @@ $(document).ready(function() {
         modal = $(".modal"),
         headerMobile = $(".header-mobile"),
         mobileMenu = $(".mobile-menu"),
-        modalElems = [
-            ".js-modal-github",
-            ".js-modal-vk",
-            ".js-modal-inst",
-            ".modal-map",
-            ".modal-toggler"
-        ];
+        btnSearch = $('.btn-search'),
+        searchInput = $('.search-input');
 
     $(".mobile-toggler").click(function() {
         if (modal.hasClass('-active')) {
@@ -66,29 +53,38 @@ $(document).ready(function() {
         $(".-from-bottom").addClass("-animated");
     }, 500);
 
-    // modalToggler.click(function() {
-    //     modal.toggleClass("-active");
-    //     for(let i = 0; i < modalElems.length ; i++) {
-    //         let timeToWait = i * 200;
-
-    //         setTimeout(function() {
-    //             $(modalElems[i]).toggleClass("-active");
-    //         }, 200 + timeToWait)
-    //     }
-    // });
-
     const clientId = 'rf51y_ZJNokGoTMgQa4Z6yOFuDpPplwSoPxyicUIMQEeC1vMWq9BlRlmwSPjGLhv';
-    const clientSecret = 'UW2zsgiVtEI5jzUVCA1TxGTbJRUwEPLDEiVuVVH9tWqQpBxdEzyiE8jUbdpWa8IZ0k0kiCs';
-    const clientToken = 'eXwmqiuCq7zzR45gqKazEMtUG0xsoE1OsvtT_ujBsxExlcGpYxEX';
+    const clientSecret = 'UW2zsgiVtEI5jzUVCA1TxGTbJRUwEPLDEiVuVVH9tWqQpBxdEzyiE8jUbdpWa8IZ0k0kiCs-Mg4myqaGRby_aA';
+    const redirectUri = '';
+    const scope = '';
+    const state = '';
 
-    const body = {
-        apikey: '5e5c3cd68b10dba06966ae7344322a25'
+    const basePath = 'https://api.genius.com/';
+    const accessTokenText = '&access_token=';
+
+    const clientToken = 'sBhmvMyC8Xn1FalOsrpSr6qYD8_tHvrUUDM18XfcVXhSb_MhutyXyoIpNjXml9yN';
+
+    let searchRequest = '';
+
+    btnSearch.click(function(){
+        searchRequest = searchInput.val();
+        sendRequest();
+    })
+
+    function sendRequest() {
+        $.get(basePath + 'search?q=' + searchRequest + accessTokenText + clientToken, function(response) {
+            for (let i = 0; i < response.response.hits.length; i++) {
+                console.log(response.response.hits[i].result);
+                $('.container-result').append(returnSongPreviewBlock(response.response.hits[i].result));
+            }
+
+        })
+
     }
 
-    $.getJSON('http://api.musixmatch.com/ws/1.1/track.search?apikey=5e5c3cd68b10dba06966ae7344322a25&q_track=Dire%20straits%20Sultans%20of%20Swing&page_size=10', function(result) {
-        console.log(result);
-    });
-
+    function returnSongPreviewBlock(songData) {
+        return '<div class="song-items"><div class="img-wrapper"><img src="' + songData.song_art_image_thumbnail_url + '"></div><div class="info-items"><span class="song-item">' +  + '</span><div class="artist-wrapper"><span>by </span><span class="artist-item">' +  + '</span></div></div></div>';
+    }
 });
 
 
