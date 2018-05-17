2ef49b124bd128b916926c84026a340d5ae19b83
ivanoff0a <ivanov-andrey15@mail.ru>
2018-05-17 18:02:04 +0300

    v.4

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
 
-    $(".owl-carousel").owlCarousel({
-        items: 1,
-        autoHeight: true,
-        animateOut: 'fadeOut',
-        nav: true
-    });
+    // $(".owl-carousel").owlCarousel({
+    //     items: 1,
+    //     autoHeight: true,
+    //     animateOut: 'fadeOut',
+    //     nav: true
+    // });
 
-    AOS.init({
-      offset: 200,
-      duration: 800,
-      easing: 'ease-in-sine',
-      delay: 300,
-    });
+    // AOS.init({
+    //   offset: 200,
+    //   duration: 800,
+    //   easing: 'ease-in-sine',
+    //   delay: 300,
+    // });
 
         $(".to-search").click(function () {
         var id  = $('.section-search'),
@@ -31,7 +31,9 @@ $(document).ready(function() {
         headerMobile = $(".header-mobile"),
         mobileMenu = $(".mobile-menu"),
         btnSearch = $('.btn-search'),
-        searchInput = $('.search-input');
+        searchInput = $('.search-input'),
+        songDiv = $('.container-result'),
+        searchError = $('.search-error');
 
     $(".mobile-toggler").click(function() {
         if (modal.hasClass('-active')) {
@@ -62,28 +64,38 @@ $(document).ready(function() {
     const basePath = 'https://api.genius.com/';
     const accessTokenText = '&access_token=';
 
-    const clientToken = 'sBhmvMyC8Xn1FalOsrpSr6qYD8_tHvrUUDM18XfcVXhSb_MhutyXyoIpNjXml9yN';
+    const clientToken = 'o1gT8mENQxIc-F1ybroPLIvESbxv3thdd163BLBHQ-gxP_x7BgCht-FwELyXYv1x';
 
     let searchRequest = '';
-
+    
+    // songDiv.html(songData); 
     btnSearch.click(function(){
         searchRequest = searchInput.val();
         sendRequest();
     })
 
     function sendRequest() {
-        $.get(basePath + 'search?q=' + searchRequest + accessTokenText + clientToken, function(response) {
-            for (let i = 0; i < response.response.hits.length; i++) {
-                console.log(response.response.hits[i].result);
-                $('.container-result').append(returnSongPreviewBlock(response.response.hits[i].result));
-            }
+        songDiv.html('');
 
+        $.get(basePath + 'search?q=' + searchRequest + accessTokenText + clientToken, function(response) {
+            if(response.response.hits.length < 1) {
+                searchError.toggleClass('-occured');
+                // setTimeout(function(){
+                //     searchError.removeClass('-occured');
+                // }, 2500);    
+            } else {
+                for (let i = 0; i < response.response.hits.length; i++) {
+                    console.log(response.response.hits[i].result);
+                    songDiv.append(returnSongPreviewBlock(response.response.hits[i].result));
+                }
+                searchError.removeClass('-occured');
+            }
         })
 
     }
 
     function returnSongPreviewBlock(songData) {
-        return '<div class="song-items"><div class="img-wrapper"><img src="' + songData.song_art_image_thumbnail_url + '"></div><div class="info-items"><span class="song-item">' +  + '</span><div class="artist-wrapper"><span>by </span><span class="artist-item">' +  + '</span></div></div></div>';
+        return '<div class="song-items"><div class="img-wrapper"><img src="' + songData.song_art_image_thumbnail_url + '"></div><div class="info-items"><span class="song-item">' + songData.title_with_featured + '</span><div class="artist-wrapper"><span class="by-item">by </span><span class="artist-item">' + songData.primary_artist.name + '</span></div></div></div>';
     }
 });
 
